31f751cd838e9a809be0e665f4429326ae61dad5
Tykillita <129893989+Tykillita@users.noreply.github.com>
2024-11-25 11:58:21 -0500

    Add files via upload

--- agendarCita.js
+++ agendarCita.js
@@ -3,33 +3,21 @@ document.getElementById("agendarCitaForm").addEventListener("submit", function(e
 
     const formData = new FormData(document.getElementById("agendarCitaForm"));
     const nombre = formData.get("nombre");
-    const edad = formData.get("edad");
-    const raza = formData.get("raza");
     const fechaCita = formData.get("fecha_cita");
     const hora = formData.get("hora");
-    const email = formData.get("email");
 
-    fetch("agendar_cita.php", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        const respuestaDiv = document.getElementById("respuesta");
+    // Mostrar pop-up
+    alert(`¡Muchas gracias por tu interés y confianza, ${nombre}!\nTe estaremos esperando el día ${fechaCita} a las ${hora} para tu cita.`);
 
-        respuestaDiv.innerHTML = `<p>${data}</p>`;
+    const respuestaDiv = document.getElementById("respuesta");
 
-        // Creacion de link para agregar a Google Calendar
-        const googleCalendarUrl = createGoogleCalendarLink(nombre, fechaCita, hora);
-        respuestaDiv.innerHTML += `<p><a href="${googleCalendarUrl}" target="_blank">Agregar a Google Calendar</a></p>`;
+    // Creación de link para agregar a Google Calendar
+    const googleCalendarUrl = createGoogleCalendarLink(nombre, fechaCita, hora);
+    respuestaDiv.innerHTML = `<p><a href="${googleCalendarUrl}" target="_blank">Agregar a Google Calendar</a></p>`;
 
-        // Agregar link para descargar el archivo ICS
-        const icsFileUrl = 'ruta_a_tu_archivo_ics/Cita_Veterinaria.ics';
-        respuestaDiv.innerHTML += `<p><a href="${icsFileUrl}" download>Descargar archivo .ics</a></p>`;
-    })
-    .catch(error => {
-        console.error("Error al agendar la cita:", error);
-    });
+    // Agregar link para descargar el archivo ICS
+    const icsFileUrl = 'ruta_a_tu_archivo_ics/Cita_Veterinaria.ics';
+    respuestaDiv.innerHTML += `<p><a href="${icsFileUrl}" download>Descargar archivo .ics</a></p>`;
 });
 
 // Función para generar el enlace de Google Calendar
